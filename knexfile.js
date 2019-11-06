c197a40b0f22d057589e4cd59bd1478700652c44
John Wells <molekewl2017@gmail.com>
2019-11-06 04:12:45 -0800

    moved files

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './car-dealer.db3'
+      filename: './data/car-dealer.db3'
     }
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
+  }
 };
