451a810027fefe7534cdb5e3fd864b4f6834f3c9
John Wells <molekewl2017@gmail.com>
2019-11-06 05:19:12 -0800

    new seed data

--- data/seeds/cars.js
+++ data/seeds/cars.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return knex('cars').insert([
         {
-          VIN: 1234,
-          Make: 'Honda',
-          Model: 'Civic',
+          VIN: 123456789,
+          Make: 'Chevy',
+          Model: 'Nova SS',
           Mileage: 1000
         }
       ]);
