b7f979ef3a3e4f30c2f6d1733ed9f5cda2d315f8
John Wells <molekewl2017@gmail.com>
2019-11-06 03:32:07 -0800

    create knex file

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './car-dealer.sqlite3'
+    }
+  },
+  useNullAsDefault: true
+};
